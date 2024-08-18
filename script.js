096a688f1e742b035e125cb9cac17c0943b56453
Faith Amarachi M. <oluebube.madu@yahoo.com>
2024-08-18 01:29:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// $(function(){
+//     menu = $('nav ul');
+
+//     $('#openup').on('click', function(e){
+//         e.preventDefault();
+//         menu.slideToggle();
+//     });
+
+//     $(window).resize(function() {
+//         var w = $(this).width();
+//         if (w > 480 && menu.is(':hidden')){
+//             menu.removeAttr('style');
+//         }
+//     });
+
+//     $('nav li').on('click', function() {
+//         var w = $(window).width();
+//         if (w < 480) {
+//             menu.slideToggle();
+//         }
+//     });
+//     $('.open-menu').height($(window).height());
+// });
